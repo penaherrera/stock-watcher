87fe048366ec6b2d161d0b61b26d8cb0e54484b6
penaherrera <carlos.cpa26@gmail.com>
2024-12-09 12:52:30 -0600

    Style modified on cards and form

--- src/components/form/LeftForm.tsx
+++ src/components/form/LeftForm.tsx
@@ -11,19 +11,25 @@ import {
   Typography,
 } from "@mui/material";
 import webSocket from "../../api/webSocket";
+import StockGraph from "../graph/StockGraph";
 
 const LeftForm = () => {
   const [symbol, setSymbol] = useState<string>("");
   const [alertInput, setAlertInput] = useState<number | undefined>(undefined);
   const [alertPrice, setAlertPrice] = useState<number | undefined>(undefined);
   const [currentPrice, setCurrentPrice] = useState<number | null>(null);
+  const [stockData, setStockData] = useState<
+    { symbol: string; price: number; timeStamp: number }[]
+  >([]);
 
   const handleWebSocketMessage = (data: any) => {
     if (data.type === "trade" && data.data) {
-      console.log("data", data);
       const trade = data.data[0];
       const price = trade.p; // Current Price
+      const timeStamp = trade.p;
       setCurrentPrice(price);
+
+      setStockData((prevData) => [...prevData, { symbol, price, timeStamp }]);
 
       if (alertPrice !== undefined) {
         if (price > alertPrice) {
@@ -48,8 +54,8 @@ const LeftForm = () => {
   };
 
   return (
-    <Box sx={{ width: "100%", maxWidth: 400, margin: "auto", padding: 3 }}>
-      <Box sx={{ marginBottom: 3 }}>
+    <Box sx={{ width: "100%", color: "white" }}>
+      <Box sx={{ marginBottom: 3, maxWidth: 400 }}>
         {symbol && currentPrice !== null && (
           <TopCard
             stockName={symbol}
@@ -59,17 +65,32 @@ const LeftForm = () => {
         )}
       </Box>
 
-      <Typography variant="h6" gutterBottom>
+      <Typography variant="h6" gutterBottom color="white">
         Select Symbol and Set Alert
       </Typography>
 
-      <FormControl fullWidth margin="normal">
-        <InputLabel id="symbol-label">Select Symbol</InputLabel>
+      <FormControl fullWidth margin="normal" sx={{ maxWidth: 500 }}>
+        <InputLabel id="symbol-label" sx={{ color: "white" }}>
+          Select Symbol
+        </InputLabel>
         <Select
           labelId="symbol-label"
           value={symbol}
           onChange={(e) => setSymbol(e.target.value)}
           label="Select Symbol"
+          sx={{
+            backgroundColor: "#333",
+            color: "white",
+            "& .MuiSelect-icon": {
+              color: "white",
+            },
+            "& .MuiInputLabel-root": {
+              color: "white",
+            },
+            "& .MuiOutlinedInput-root": {
+              borderColor: "#888",
+            },
+          }}
         >
           <MenuItem value="BINANCE:BTCUSDT">BTC-USDT</MenuItem>
           <MenuItem value="BINANCE:ETHUSDT">ETH-USDT</MenuItem>
@@ -84,6 +105,22 @@ const LeftForm = () => {
         value={alertInput || ""}
         onChange={(e) => setAlertInput(Number(e.target.value))}
         type="number"
+        slotProps={{
+          inputLabel: {
+            style: { color: "white" },
+          },
+        }}
+        sx={{
+          maxWidth: 500,
+          margin: 2,
+          color: "white",
+          input: {
+            color: "white",
+          },
+          fieldset: {
+            borderColor: "white",
+          },
+        }}
       />
 
       <Box sx={{ display: "flex", gap: 2, marginTop: 2 }}>
@@ -92,6 +129,13 @@ const LeftForm = () => {
           color="primary"
           onClick={handleSetAlert}
           disabled={alertInput === undefined || alertInput === null}
+          sx={{
+            backgroundColor: "#007bff",
+            color: "white",
+            "&:disabled": {
+              backgroundColor: "#555",
+            },
+          }}
         >
           Set Alert
         </Button>
@@ -100,18 +144,26 @@ const LeftForm = () => {
           color="secondary"
           onClick={handleClearAlert}
           disabled={alertPrice === undefined}
+          sx={{
+            borderColor: "#aaa",
+            color: "white",
+            "&:disabled": {
+              borderColor: "#555",
+              color: "#555",
+            },
+          }}
         >
           Clear Alert
         </Button>
       </Box>
 
-      <Box sx={{ marginTop: 3 }}>
-        {alertPrice !== undefined && currentPrice !== null && (
-          <Typography color={currentPrice > alertPrice ? "green" : "red"}>
-            The current price is {currentPrice > alertPrice ? "above" : "below"}{" "}
-            the alert value of ${alertPrice}.
-          </Typography>
-        )}
+      <Box
+        sx={{
+          marginTop: 3,
+          maxWidth: "75%",
+        }}
+      >
+        <StockGraph stockData={stockData} />
       </Box>
     </Box>
   );
